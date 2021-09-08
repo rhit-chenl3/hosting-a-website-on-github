fba31bf127a964382f0738827aeec54a9e6b37c4
Leo Chen <chenl3@rose-hulman.edu>
2021-09-08 18:13:33 -0400

    Added event listeners

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const image = document.querySelector('img');
+
+function mouseoverImage(){
+    image.style.animationDirection = "scaleX(-1)";
+}
+
+function mouseleaveImage(){
+    image.style.removeProperty(animationDirection);
+}
+
+image.addEventListener('mouseenter', mouseoverImage);
+image.addEventListener('mouseleave', mouseleaveImage);
