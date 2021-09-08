d15424adae3714ae4b3d2ca56e9b6e084372e171
Leo Chen <chenl3@rose-hulman.edu>
2021-09-08 18:27:48 -0400

    updated index script and style

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "My WIXSITE";
 document.querySelector("body").appendChild(h2);
 
 const image = document.querySelector('img');
 
 function mouseoverImage(){
-    image.style.animationDirection = "scaleX(-1)";
+    image.style.transform = "scaleX(-1)";
 }
 
 function mouseleaveImage(){
-    image.style.removeProperty(animationDirection);
+    image.style.removeProperty(transform);
 }
 
 image.addEventListener('mouseenter', mouseoverImage);
